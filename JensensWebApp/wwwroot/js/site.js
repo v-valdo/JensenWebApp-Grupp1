1382b5a9716a32dd922e5a2e8c109a31431e80f6
BennyAhlin <Benny.Ahlin@hotmail.com>
2024-05-31 17:59:47 +0200

    changed size of robot and perc of movement

--- JensensWebApp/wwwroot/js/site.js
+++ JensensWebApp/wwwroot/js/site.js
@@ -4,7 +4,7 @@ window.addEventListener('scroll', function () {
     const scrollTop = window.scrollY;
     const documentHeight = document.documentElement.scrollHeight - window.innerHeight;
     const scrollPercent = scrollTop / documentHeight;
-    const movePercent = scrollPercent * 95;
+    const movePercent = scrollPercent * 92;
     robot.style.transform = `translateX(${movePercent}vw)`;
 });
 
