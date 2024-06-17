accd8c9716ee3eac7b7a398a65c87b988c019109
matilda002 <matilda0817@hotmail.com>
2024-06-17 16:15:05 +0200

    added a confetti effect
    when user submits form, confetti pops out :D

--- JensensWebApp/wwwroot/js/site.js
+++ JensensWebApp/wwwroot/js/site.js
@@ -9,3 +9,20 @@ window.addEventListener('scroll', function () {
 });
 
 
+document.getElementById('contactForm').addEventListener('submit', function (event) {
+    event.preventDefault(); // making sure confetti won't activate in a incomplete form
+
+    // Calculating the position of the button
+    const submitButton = document.getElementById('submitButton');
+    const rect = submitButton.getBoundingClientRect();
+    const originX = (rect.left + rect.right) / 2 / window.innerWidth;
+    const originY = (rect.top + rect.bottom) / 2 / window.innerHeight;
+
+    // The confetti effects
+    confetti({
+        particleCount: 100,
+        spread: 70,
+        origin: { x: originX, y: originY },
+        colors: ['#0056b3', '#e6e04b', '#2a2a2a', '#fcf9a1'] 
+    });
+});
